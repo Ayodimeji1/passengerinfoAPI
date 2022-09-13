e876a76e94621bbaeafca533db1d2711ec36e74a
Ayodimeji Kadri <ayodimejia1@gmail.com>
2022-09-14 00:13:01 +0100

    Changes to final commit

--- passenger/models.py
+++ passenger/models.py
@@ -6,9 +6,10 @@ from django.core.validators import MaxValueValidator
 
 class Passenger(models.Model):
     id = models.IntegerField(primary_key=True, validators=[MaxValueValidator(999999999)])
-    name = models.CharField(max_length=100)
-    age = models.IntegerField(validators=[MaxValueValidator(99)])
-    sex = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    departure = models.CharField(max_length=100)
+    arrival = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name+self.age+self.sex
--- passenger/serializers.py
+++ passenger/serializers.py
@@ -5,4 +5,4 @@ from passenger.models import Passenger
 class PassengerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Passenger
-        fields = ['id','name', 'age', 'sex']
+        fields = ['id','first_name','last_name','arrival','departure']
